6b69bb8518cfa752fb88c6951f483506cc96ef4e
Andrew Lapteff <this.is.amigogo@gmail.com>
2024-04-03 10:19:53 +0300

    Add a test report

--- tests/main_test.py
+++ tests/main_test.py
@@ -2,7 +2,11 @@ import os
 
 import pytest
 
-from main import count_sentences, count_words, count_words_and_sentences
+from text_processing.text_processing import (
+    count_sentences,
+    count_words,
+    count_words_and_sentences,
+)
 
 
 @pytest.fixture
